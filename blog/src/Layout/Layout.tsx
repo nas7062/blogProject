ffc6bd0836b08da4501c40d9870a2c4dbe5f6087
10012 <nas7062@naver.com>
2025-03-27 22:56:04 +0900

    중첩 레이아웃 생성

---
+++ blog/src/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import { Outlet } from "react-router-dom";
+import CategorySection from "../components/CategorySection";
+import SideBar from "../components/Sidebar";
+import PostList from "../components/PostLIst";
+
+function Layout() {
+  return (
+    <div className="flex">
+      <SideBar />
+      <div className="flex-1 flex-col">
+        <CategorySection />
+        <main>
+          <Outlet/>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
