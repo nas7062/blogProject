db6317ca3706e3afae280f66ff6e4f0e13e5519d
kim min seok <nas7062@naver.com>
2025-03-28 20:23:56 +0900

    Post Css grid로 바꿈'

--- blog/src/components/Post.tsx
+++ blog/src/components/Post.tsx
@@ -1,12 +1,12 @@
 import Image from "../assets/profile.jpg";
 function Post() {
   return (
-    <div className=" w-4/6 mx-auto border-t border-b py-4 border-gray-200 flex justify-between mt-10">
+    <div className="grid grid-flow-col grid-cols-2 grid-template-columns: 1fr 200px; relative left-12 place-items-center; border-y py-4 border-gray-200  mt-10">
       <div className="flex flex-col ">
         <h3 className="text-3xl">Title</h3>
         <p className="mt-2">createTime / Category</p>
           <p className="mt-6 w-3/4 break-words">
-            asdsadsadasdsadsadsadsadsadasdsadsadasdsadsadsadsadsadasdsadsadasdsadsadsadsadsadasdsadsadasdsadsadsadsadsad
+            asdsadsadasdsadsadsadsadsadasdsadsdsadsadasdsadsadsadsadasdsa
           </p>
       </div>
       <img src={Image} alt="image" className="w-72 max-w-full object-contain flex-shrink-0" />
