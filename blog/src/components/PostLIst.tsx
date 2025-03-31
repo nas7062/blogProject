9dcea34c5c3a673d03ed2c6184788a5e414d9fcc
10012 <nas7062@naver.com>
2025-03-31 22:17:31 +0900

    카테고리 Post불러오는 api 생성

--- blog/src/components/PostLIst.tsx
+++ blog/src/components/PostLIst.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Post from "./Post";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
 export interface PostData {
   id: number;
   title: string;
@@ -12,26 +13,31 @@ export interface PostData {
 
 function PostList() {
   const [posts, setPosts] = useState<PostData[]>([]);
+  const location = useLocation();
 
+  const queryParams = new URLSearchParams(location.search);
+  const category = queryParams.get("category");
   useEffect(() => {
+    const url = category
+      ? `http://localhost:8080/getPostByCategory/${category}`
+      : "http://localhost:8080/getAllPost";
     axios
-      .get("http://localhost:8080/getPost") // 게시글 데이터 가져오기
+      .get(url)
       .then((response) => {
-        setPosts(response.data); // 상태 업데이트
-        console.log(response.data);
+        setPosts(response.data);
       })
       .catch((error) => {
         console.error("데이터 가져오기 실패:", error);
       });
-  }, []);
-  return(
+  }, [category]);
+  return (
     <div className="mt-10">
-    {posts.length === 0 ? (
-      <p className="text-center text-gray-500">게시글이 없습니다.</p>
-    ) : (
-      posts.map((post) => <Post key={post.id} post={post} />) // Post 컴포넌트로 전달
-    )}
-  </div>
+      {posts.length === 0 ? (
+        <p className="text-center text-gray-500 prose prose-lg">게시글이 없습니다.</p>
+      ) : (
+        posts.map((post) => <Post key={post.id} post={post} />) // Post 컴포넌트로 전달
+      )}
+    </div>
   );
 }
 
