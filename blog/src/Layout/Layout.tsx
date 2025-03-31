ddd013146dd215275939f36d80463669a4ca380a
10012 <nas7062@naver.com>
2025-03-31 22:13:03 +0900

    getCategoryAll api 추가

--- blog/src/Layout/Layout.tsx
+++ blog/src/Layout/Layout.tsx
@@ -1,13 +1,31 @@
 import { Outlet } from "react-router-dom";
 import CategorySection from "../components/CategorySection";
 import SideBar from "../components/Sidebar";
+import axios from "axios";
+import { useEffect, useState } from "react";
+interface CategoryPostCount {
+  category: string;
+  postCount: number;
+}
 
 function Layout() {
+  const [categories, setCategories] = useState<CategoryPostCount[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/getCategoryAll")
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
   return (
     <div className="flex">
-      <SideBar />
+      <SideBar categories={categories}/>
       <div className="flex-1 flex-col ">
-        <CategorySection />
+        <CategorySection  />
         <main>
           <Outlet/>
         </main>
