2bdca376cc855e5ae5cd5db35c4c3946c062ab17
10012 <nas7062@naver.com>
2025-03-31 22:18:03 +0900

    link태그 및 layout에서 props로 받아옴

--- blog/src/components/Sidebar.tsx
+++ blog/src/components/Sidebar.tsx
@@ -1,40 +1,32 @@
 import { Link } from "react-router-dom";
 import ProfileImage from "../assets/profile.jpg";
-function SideBar() {
+interface SideBarProps {
+  categories: { category: string; postCount: number }[];
+}
+function SideBar({ categories }:SideBarProps ) {
   return (
     <div className="max-w-64 bg-green-200 h-screen sticky top-0 float-left">
       <div className="w-3/5 mx-auto text-center ">
         <img src={ProfileImage} alt="text" className="mt-44 border-r-0 rounded-full" />
         <p className="mt-4">프론트엔드 개발자</p>
-        <p className="mt-2"><Link to="/">10012</Link></p>
+        <p className="mt-2">
+          <Link to="/">10012</Link>
+        </p>
         <div className="mt-4">
           <span>전체 10</span>
           <span className="ml-10">오늘 5</span>
         </div>
       </div>
       <div className="mt-10 w-4/5 mx-auto">
-        <p>카테고리(전체개수)</p>
+        <p><Link to="/">카테고리(전체)</Link></p>
         <ul className="flex flex-col gap-4 mt-4">
-          <li>
-            <span>title</span>
-            <span>(1)</span>
-          </li>
-          <li>
-            <span>title</span>
-            <span>(1)</span>
-          </li>
-          <li>
-            <span>title</span>
-            <span>(1)</span>
-          </li>
-          <li>
-            <span>title</span>
-            <span>(1)</span>
-          </li>
-          <li>
-            <span>title</span>
-            <span>(1)</span>
-          </li>
+          {categories.map((category, idx) => (
+            <li key={idx} className="cursor-pointer">
+              <Link to={`/?category=${category.category}`}>
+                {category.category} ({category.postCount})
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
