595c2e7a2ef53ea72d17f4169fcedd17f263542d
10012 <nas7062@naver.com>
2025-04-05 20:52:11 +0900

    모달 생성 및 배경 클릭시 닫기 추가

--- blog/src/components/Modal.tsx
+++ blog/src/components/Modal.tsx
@@ -1,9 +1,34 @@
+import React from "react";
 
-function Modal() {
+interface ModalProps {
+  onClose: () => void;
+};
 
-  return(
-    <div>
-      
+const Modal: React.FC<ModalProps> = ({onClose}) =>{
+  const handleBackgroundClick = () => {
+    onClose(); // 배경 클릭하면 모달 닫힘
+  };
+
+  const handleModalClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation(); // 모달 내부 클릭은 닫힘 방지
+  };
+
+  return (
+    <div className="w-full h-screen bg-gray-100 relative " onClick={handleBackgroundClick}>
+      <div onClick={handleModalClick} className="absolute w-96 h-72 bg-white top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg p-6">
+        <div className="text-center flex flex-col gap-4">
+          <h2 className="text-2xl mt-10">로그인</h2>
+          <div className="w-80">
+            <label htmlFor="">이메일:</label>
+            <input type="text" className="border w-64" />
+          </div>
+          <div className="w-80">
+            <label htmlFor="">비밀번호:</label>
+            <input type="password" name="" id="" className="border w-60" />
+          </div>
+          <button className="bg-lime-400 hover:bg-lime-500 text-white py-2 rounded mt-2">로그인하기</button>
+        </div>
+      </div>
     </div>
   );
 }
