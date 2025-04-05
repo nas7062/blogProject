46faab07ef23244bb76dab0f49d59375f6fe5b06
10012 <nas7062@naver.com>
2025-04-05 20:52:47 +0900

    모달 open 기능 추가

--- blog/src/components/CategorySection.tsx
+++ blog/src/components/CategorySection.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import Modal from "./Modal";
 
 function CategorySection() {
-  const [login, setlogin] = useState<boolean>(true);
+  const [login, setlogin] = useState<boolean>(false);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const location = useLocation();
   
   const queryParams = new URLSearchParams(location.search);
@@ -14,9 +16,11 @@ function CategorySection() {
         <h2 className="absolute  left-1/2 top-32 text-4xl  transform -translate-x-1/2">{category ? category :`전체보기`}</h2>
       </div>
       <h3 className="float-right mr-4 mt-4 bg-lime-200 px-1.5 py-1.5  rounded-xl cursor-pointer">
-        {login ? <Link to="/write"> 새 글 작성</Link> : <p>로그인</p>}
+         <Link to="/write"> 새 글 작성</Link> : <p onClick={()=>setIsOpen(true)}>로그인</p>
       </h3>
+      {isOpen && <Modal  onClose={() => setIsOpen(false)} />}
     </div>
+    
   );
 }
 
