fec1509d4f20837e93f43cda1fa2623ad7338b7a
10012 <nas7062@naver.com>
2025-04-09 22:36:23 +0900

    detail page 마크다운 적용

--- blog/src/pages/PostDetail.tsx
+++ blog/src/pages/PostDetail.tsx
@@ -1,15 +1,17 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { PostData } from '../components/PostLIst';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { PostData } from "../components/PostLIst";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 const PostDetail = () => {
-  const { id } = useParams<{ id: string  }>();
-  const [post,setPost] = useState<PostData | null>(null);
+  const { id } = useParams<{ id: string }>();
+  const [post, setPost] = useState<PostData | null>(null);
   const numId = Number(id);
-  useEffect(()=> {
+  useEffect(() => {
     axios
-      .get(`http://localhost:8080/getPostById/${numId}`,{
-        params:{numId},
+      .get(`http://localhost:8080/getPostById/${numId}`, {
+        params: { numId },
       })
       .then((response) => {
         console.log(response.data);
@@ -18,14 +20,22 @@ const PostDetail = () => {
       .catch((error) => {
         console.error("데이터 가져오기 실패:", error);
       });
-  },[])
+  }, []);
 
   return (
-    <div>
-      {post? <p>{post.title}</p> : <div>"포스트 없음"</div>}
-      
+    <div className="w-2/4 mx-auto border-red-200 border">
+      {post ? (
+        <div className="flex flex-col gap-2">
+          <h1>{post.title}</h1>
+          <span>{post.createdAt.slice(0,10)}</span>
+          <span>{post.category}</span>
+          <p><ReactMarkdown remarkPlugins={[remarkGfm]}>{post.content}</ReactMarkdown></p>
+        </div>
+      ) : (
+        <div>"포스트 없음"</div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PostDetail
+export default PostDetail;
